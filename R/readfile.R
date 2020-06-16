49ea7a42b9e626595346c0885cf741f06b3eed58
qwangcode <qiurong.wang@gmail.com>
2020-06-16 09:58:43 -0600

    Edits made

--- R/readfile.R
+++ R/readfile.R
@@ -1,7 +1,7 @@
 #' Funtion: rf1 to read a csv file and display an excerpt
 #' @export
 
-rf1 <- function(inflie){
+rf1 <- function(infile){
 	store <- read.csv(infile, header=T)
 	print(paste("The input file is: ", infile))
 	print(paste("Here are first few lines of this file "))
